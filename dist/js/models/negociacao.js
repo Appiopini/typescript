0b879b4b480fb2fdfc44fc66db317772e417f196
Appiopini <appiopini@gmail.com>
2023-05-08 13:48:15 -0300

    new code

---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,29 @@
+export class Negociacao {
+     #data;
+     #quantidade;
+     #valor;
+     
+     contructor (data, quantidade, valor) {
+          this.#data = data;
+          this.#quantidade = quantidade;
+          this.#valor = valor;
+     }
+
+     get data(){
+          this.#data;
+     }
+
+     get quantidade(){
+          this.#quantidade;
+     }
+
+     get valor(){
+          this.#valor;
+     }
+
+     get volume(){
+          return this.#quantidade * this.#valor;
+     }
+}
+
+
